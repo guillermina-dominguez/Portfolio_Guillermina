e35834eb623ce988a4f128ff9597d160a6d8fa78
Guillermina Dominguez <dominguez.guillermina.andrea@gmail.com>
2022-08-23 01:22:21 -0300

    Descarga de Spring Initializr y creación de carpeta de Back End

---
+++ Portfolio/BackEnd/src/main/java/com/portfolio/Guillermina/GuillerminaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Guillermina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuillerminaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuillerminaApplication.class, args);
+	}
+
+}
