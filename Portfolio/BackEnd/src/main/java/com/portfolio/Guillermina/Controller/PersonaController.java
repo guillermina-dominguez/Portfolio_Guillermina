9a8d3e3bbce3ccc20db4a0933116c1b870bc0d06
Guillermina Dominguez <dominguez.guillermina.andrea@gmail.com>
2022-08-25 00:43:40 -0300

    Implementación de Backend en Frontend y routing

--- Portfolio/BackEnd/src/main/java/com/portfolio/Guillermina/Controller/PersonaController.java
+++ Portfolio/BackEnd/src/main/java/com/portfolio/Guillermina/Controller/PersonaController.java
@@ -8,6 +8,7 @@ import com.portfolio.Guillermina.Entity.Persona;
 import com.portfolio.Guillermina.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author usuario
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
 
